10dc49bd7e57788c6b5a958daecc8dd7b5194c81
Steve Lillis <stevelillis@outlook.com>
2014-11-12 13:29:27 +0000

    Updated assembly info

--- Outsorcery/Properties/AssemblyInfo.cs
+++ Outsorcery/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Outsorcery")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Async distributed computing for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Steve Lillis")]
 [assembly: AssemblyProduct("Outsorcery")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
